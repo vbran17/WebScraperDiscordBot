275d4b515d792b255b2cf45f2d4350b97f87801c
vbran17 <37545121+vbran17@users.noreply.github.com>
2024-03-31 00:57:18 -0400

    Update search_function.py
    Switched to letterbox site scraping

--- search_function.py
+++ search_function.py
@@ -4,11 +4,11 @@ from bs4 import BeautifulSoup
 class RunWeb:
   def __init__(self):
     self.headers = {'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36'}
-    self.url = 'https://secure.runescape.com/m=itemdb_rs/top100?list=1'
+    self.url = 'https://letterboxd.com/search/films/'
 
   def key_words_search_words(self, user_message):
     words = user_message.split()[1:]
-    keywords = '+'.join(words)
+    keywords = '-'.join(words)
     search_words = ' '.join(words)
     return keywords, search_words
 
@@ -20,9 +20,14 @@ class RunWeb:
     return result_links
 
   def send_link(self, result_links, search_words): 
+    baseurl = 'https://letterboxd.com'
     send_link = set()
+    count = 0
     for link in result_links:
         text = link.text.lower()
         if search_words in text:  
-          send_link.add(link.get('href'))
+          send_link.add(baseurl + link.get('href'))
+          count += 1
+        if count == 5:
+          break
     return send_link
